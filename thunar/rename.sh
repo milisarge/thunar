a2b9eb5815002b07f3df40954b72066c3070f018
barrydegraaff <info@barrydegraaff.tk>
2016-02-10 10:37:46 +0100

    implement cancel button and show close confirmation after mv

--- thunar/rename.sh
+++ thunar/rename.sh
@@ -19,9 +19,19 @@ fi
 INIT=""
 input=$(whiptail --topleft --inputbox "Rename $1 to:" 13 78 $INIT --title "Rename file or folder" 3>&1 1>&2 2>&3)
  
+if [ "$?" = "1" ]
+then
+   echo "cancelled by user"
+   exit 0
+fi  
+ 
 if [[ -z "$input" ]]; then
    exit 0  
 fi
 mv  "$1" "$input"
+
+
+echo "All done, please check if there are any errors in the terminal. And hit enter key to exit."
+read end;
 
 exit 0
